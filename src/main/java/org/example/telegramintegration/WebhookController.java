a8a5cba8051df84f60a88602cfcc2b934874677b
Đặng Tấn <tandang264@gmail.com>
2025-07-17 21:47:40 +0700

    change to post

--- src/main/java/org/example/telegramintegration/WebhookController.java
+++ src/main/java/org/example/telegramintegration/WebhookController.java
@@ -3,6 +3,7 @@ package org.example.telegramintegration;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 public class WebhookController {
 
-    @GetMapping("/webhook")
+    @PostMapping("/webhook")
     public ResponseEntity<Boolean> webhook(@RequestBody String data) {
         // This endpoint is used to set up the webhook for Telegram
         // You can add logic here to handle the webhook setup if needed
